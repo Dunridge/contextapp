c99122fab7be7502a0e19830969e01bdb48368f6
Maksym Polinka <Polinka7max@gmail.com>
2020-04-11 19:32:19 +0300

    added context and a provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import BookList from './components/BookList'; 
+import BookList from './components/BookList';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <BookList/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeContextProvider>
+                <Navbar />
+                <BookList />
+            </ThemeContextProvider>
+        </div>
+    );
 }
 
 export default App;
