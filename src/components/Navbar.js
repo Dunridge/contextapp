9ff52b4513a702739dfcf61f46c8a4eef01a785a
Maksym Polinka <Polinka7max@gmail.com>
2020-04-12 08:12:21 +0300

    added a theme context consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends React.Component {
-    render() { 
-        return ( 
-            <nav>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-         );
+    render() { //this way of consuming context is easier
+        return ( // this works in functional components as well 
+            <ThemeContext.Consumer> 
+                {(context) => {
+                    const { isLightTheme, light, dark } = context;
+                    const theme = isLightTheme ? light : dark;
+                    return (
+                        <nav
+                            style={{
+                                background: theme.ui,
+                                color: theme.syntax,
+                            }}
+                        >
+                            <h1>Context App</h1>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    );
+                }}
+            </ThemeContext.Consumer>
+        );
     }
 }
- 
+
 export default Navbar;
